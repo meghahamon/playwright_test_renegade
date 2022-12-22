9580d2e7ae25f19790198ff6faabc82132e8f337
megha <megha@hamon.in>
2022-12-22 12:29:42 +0530

    adding the test cases for adding documents in lead

--- test_lead_adddocument.py
+++ test_lead_adddocument.py
@@ -6,7 +6,7 @@ import test_login
 
 
 def test_lead_adddocument(page: Page):
-    test_login.test_renegade_login(page)
+    # test_login.test_renegade_login(page)
 
     page.get_by_role("img", name="logo").first.click()
     page.get_by_role("heading", name="My Book").click()
